f68a8f6493ee17a2cd881af656292eb52a8a7435
Ishan Bhardwaj <59660334+IB-14@users.noreply.github.com>
2020-10-11 08:28:32 +0530

    Added functions to app.js

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ let tempImgs = document.querySelector('.temp-imgs');
 let prevtext = document.querySelector('#prev-text');
 const previewBox = document.getElementById('Preview-box');
 let lowerbuts = document.getElementById('lowerbuts');
+let fileDiv = document.getElementById('fileupload');
+// document.getElementById("upup").addEventListener("click",addingConvertBut);
 let reqTemplateName;
-var count = 0;
+var count=0;
 
 var certificateList = []
 for(i=0; i<52; i++) {
@@ -30,18 +32,36 @@ function certNameHandler(element) {
 	certificateName = certificateList[element.id-1];
 	previewBox.style.backgroundImage = `url(${element.src})`;
 	prevtext.innerHTML = `<i>This looks nice </i>&#9749;`
-	lowerbuts.innerHTML = `<label class="button" onclick="addingDownloadBut()">Select this theme</label>`
-	
+	lowerbuts.innerHTML = `<label class="button" onclick="addingDownloadBut()">Select this template</label>`
+	count=0;
 	reqTemplateName=certificateName;
 	
 }
 
 function addingDownloadBut() {
+	count=1;
 	console.log(reqTemplateName);
-	lowerbuts.innerHTML = `<label class="button" onclick="returningTemplateName()">Download Your File</label>`
+	console.log(count);
+	lowerbuts.innerHTML = `<label class="button" onclick="alert2()">Download Your File</label>`
 }
 
-function returningTemplateName() {
-	
+// function addingConvertBut() {
+// 	fileDiv.innerHTML = `<label class="button" >CONVERT</label>`
+// }
+
+function alert1() {
+	if( count===0) {
+		alert("Select Template First")
+		event.preventDefault();
+	}
+	else {
+		count=2;
+	}
 }
 
+function alert2() {
+	if( count != 2) {
+		alert("Upload your excel file first")
+		event.preventDefault();
+	}
+}
