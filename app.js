451cf72256e97a870f257e6c6aeb3b5bde6b6ad3
Ishan Bhardwaj <59660334+IB-14@users.noreply.github.com>
2020-10-11 06:03:26 +0530

    Updated Frontend

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 let tempImgs = document.querySelector('.temp-imgs');
 let prevtext = document.querySelector('#prev-text');
 const previewBox = document.getElementById('Preview-box');
-
+let lowerbuts = document.getElementById('lowerbuts');
+let reqTemplateName;
+var count = 0;
 
 var certificateList = []
 for(i=0; i<52; i++) {
 	certificateList[i] = `cert${i+1}`;
 }
-
-console.log(certificateList);
-
 
 let sourcelist = []
 for(i=1; i<certificateList.length+1; i++) {
@@ -28,7 +27,21 @@ for (i = 1; i < certificateList.length+1; ++i) {
 
 prevtext.innerHTML = '';
 function certNameHandler(element) {
-	certificateName = certificateList[element.id];
+	certificateName = certificateList[element.id-1];
 	previewBox.style.backgroundImage = `url(${element.src})`;
 	prevtext.innerHTML = `<i>This looks nice </i>&#9749;`
+	lowerbuts.innerHTML = `<label class="button" onclick="addingDownloadBut()">Select this theme</label>`
+	
+	reqTemplateName=certificateName;
+	
 }
+
+function addingDownloadBut() {
+	console.log(reqTemplateName);
+	lowerbuts.innerHTML = `<label class="button" onclick="returningTemplateName()">Download Your File</label>`
+}
+
+function returningTemplateName() {
+	
+}
+
